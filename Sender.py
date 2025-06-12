58bcdfd8134364c22a83d03c4bcfc31874f268ca
Pooja2301ydv <145289623+Pooja2301ydv@users.noreply.github.com>
2025-06-12 17:45:06 +0530

    Delete Sender.py

--- Sender.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-ip_add="172.20.10.2"
-port_num=1234
-complete = (ip_add, port_num)
-msg=input("What message you want to deliver")
-encode_msg=msg.encode('ascii')
-s.sendto(encode_msg, complete)
